7853b762797eae2cee54fe11ace0eb5b8cc44e75
Tomonori Tamura <tmrtmnr@gmail.com>
2019-01-17 02:03:58 +0900

    しゃべりを英語に。 API呼び出し処理はクエリストリングを正しく作るように。 スクロール処理をログ追加時に自動的に行うように変更。

--- js/vue.js
+++ js/vue.js
@@ -5,6 +5,22 @@
 // TODO 中国語問題（相談）
 // TODO レスポンスを少し遅らせたい
 // TODO ロゴ
+var API_URL='https://script.google.com/macros/s/AKfycbweJFfBqKUs5gGNnkV2xwTZtZPptI6ebEhcCU2_JvOmHwM2TCk/exec';
+var buildQuery = function(data){
+  var q = [];
+  for (var key in data) {
+    if (data.hasOwnProperty(key)) {
+      q.push(`${key}=${encodeURIComponent(data[key])}`);
+    }
+  }
+  return q.join('&');
+}
+
+var say = function (text, lang) {
+  var utter = new SpeechSynthesisUtterance(text);
+  utter.lang = lang;
+  speechSynthesis.speak(utter);
+}
 
 var app = new Vue({
   el: "#app",
@@ -12,7 +28,8 @@ var app = new Vue({
     message: "", // 入力データの一時変数
     logs: [], // 入力データを詰めていくlist
     inputVoice: false, // 音声入力が行われた時trueに
-    recognitionText: "音声入力"// ボタンのラベル
+    recognitionText: "音声入力", // ボタンのラベル
+    outputLang: 'en',
   },
   methods: {
     // 送信ボタンを押された場合の処理
@@ -23,20 +40,20 @@ var app = new Vue({
       }
       this.pushLogs("You", this.message, "img/user.png");
       // 翻訳モード時の処理
-      this.scrollLogs(1);
+      var qs = buildQuery({
+        text: this.message,
+        source: 'ja',
+        target: this.outputLang,
+      });
       axios
-        .get(
-          `https://script.google.com/macros/s/AKfycbweJFfBqKUs5gGNnkV2xwTZtZPptI6ebEhcCU2_JvOmHwM2TCk/exec?text=${
-            this.message
-          }&source=ja&target=en`
-        )
-        .then(response =>
-          this.pushLogs("Chat bot", response.data, "img/route66.png")
-        )
+        .get(`${API_URL}?${qs}`)
+        .then(response =>{
+          this.pushLogs("Chat bot", response.data, "img/route66.png");
+          say(response.data, this.outputLang);
+        })
         .catch(error => {
           console.log(error);
-        })
-        .finally(() => this.scrollLogs(1));
+        });
       this.message = "";
     },
     // 音声入力の処理
@@ -44,55 +61,46 @@ var app = new Vue({
       this.inputVoice = true;
       const speech = new webkitSpeechRecognition();
       speech.lang = "ja-JP";
-      speech.start();
       speech.onresult = e => {
         speech.stop();
         if (e.results[0].isFinal) {
           // 翻訳モードの時の処理
           var voiceText = e.results[0][0].transcript;
           this.pushLogs("You", voiceText, "img/user.png");
-          this.scrollLogs(1);
-          speech.lang = "en-US";
+          var qs = buildQuery({
+            text: voiceText,
+            source: 'ja',
+            target: this.outputLang
+          });
           axios
-            .get(
-              `https://script.google.com/macros/s/AKfycbweJFfBqKUs5gGNnkV2xwTZtZPptI6ebEhcCU2_JvOmHwM2TCk/exec?text=${voiceText}&source=ja&target=en`
-            )
-            .then(response =>
-              this.pushLogs("Chat bot", response.data, "img/route66.png")
-            )
+            .get(`${API_URL}?${qs}`)
+            .then(response => {
+              this.pushLogs("Chat bot", response.data, "img/route66.png");
+              say(response.data, this.outputLang);
+            })
             .catch(error => {
               console.log(error);
-            })
-            .finally(() =>
-              speechSynthesis.speak(
-                new SpeechSynthesisUtterance(this.logs.slice(-1)[0].text)
-              )
-            );
-          // TODO 原因追求
-          this.scrollLogs(1100);
+            });
         }
         false;
       };
-
       speech.onend = () => {
         this.recognitionText = "音声入力";
       };
-
       speech.onsoundstart = () => {
         this.recognitionText = "Processing";
       };
-
       speech.onsoundend = () => {
         this.recognitionText = "Waiting";
       };
+      speech.start();
     },
-    scrollLogs: function(time) {
-      Vue.nextTick(()=>{
-        this.$refs.scrollp.scrollBy(0, this.$refs.scrollp.offsetHeight);
-      });
-    },
+    
     pushLogs: function(speaker, text, img) {
       this.logs.push({ speaker: speaker, text: text, img: img });
+      Vue.nextTick(()=>{
+        this.$refs.scrollp.scrollTop = this.$refs.scrollp.scrollHeight;
+      });
     }
   }
 });
